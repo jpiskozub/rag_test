2ab45bccd0f6a86c34504928f92a113b11ce1cfb
KubaP <jpiskozub@gmail.com>
2025-03-03 15:53:17 +0100

    Section splitter added

--- rag.py
+++ rag.py
@@ -4,6 +4,10 @@ from pprint import pprint
 import json
 from pathlib import Path
 import nltk
+import re
+
+
+
 from langchain_community.document_loaders import TextLoader
 from langchain_community.document_loaders import PyPDFLoader
 from langchain_community.document_loaders import PyMuPDFLoader
@@ -24,3 +28,42 @@ loader = PyPDFLoader(pdf_url)
 pages = loader.load_and_split()
 
 #%%
+latex_text = r"""
+\documentclass{article}
+
+\begin{document}
+
+\maketitle
+
+\section{Introduction}
+
+Large language models (LLMs) are a type of machine learning model that can be trained on vast amounts of text data to generate human-like language. In recent years, LLMs have made significant advances in various natural language processing tasks, including language translation, text generation, and sentiment analysis.
+
+\subsection{History of LLMs}
+
+The earliest LLMs were developed in the 1980s and 1990s, but they were limited by the amount of data that could be processed and the computational power available at the time. In the past decade, however, advances in hardware and software have made it possible to train LLMs on massive datasets, leading to significant improvements in performance.
+
+\subsection{Applications of LLMs}
+
+LLMs have many applications in the industry, including chatbots, content creation, and virtual assistants. They can also be used in academia for research in linguistics, psychology, and computational linguistics.
+
+\end{document}
+"""
+# %%
+
+
+for i, page in enumerate(pages):
+    print(f"\n--- Page {i+1} ---\n")
+    print(page.page_content)
+
+# Section splitter
+sections = re.split(r'\\section{(.+?)}', latex_text)
+parsed_sections = {}
+for i in range(1, len(sections), 2):
+    title = sections[i]
+    content = sections[i + 1].strip()
+    parsed_sections[title] = content
+
+
+pprint(parsed_sections)
+
