78e9bbcc5f59aa8ee75b1ee4fc0eb4839516b18d
KubaP <jpiskozub@gmail.com>
2025-03-08 09:37:36 +0100

    Refactors document processing and retrieval functions
    Implements modular functions for document loading, text splitting, and vector database creation.
    
    Enhances maintainability and readability of the codebase by restructuring logic into separate functions.
    
    Improves the overall functionality of the retrieval system.
    
    Relates to improving the document processing pipeline.

--- rag.py
+++ rag.py
@@ -1,4 +1,3 @@
-#%%
 #IMPORTS
 from pprint import pprint
 
@@ -22,126 +21,78 @@ from langchain_community.vectorstores import Chroma
 from langchain_community.document_loaders import PyPDFLoader
 from langchain.chains import RetrievalQA
 
-# %%
-pdf_url = "https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/Q81D33CdRLK6LswuQrANQQ/instructlab.pdf"
+def document_loader(file):
+    loader = PyPDFLoader(file.name)
+    loaded_document = loader.load()
+    return loaded_document
 
-loader = PyPDFLoader(pdf_url)
+## Text splitter
+def text_splitter(data):
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size=1000,
+        chunk_overlap=50,
+        length_function=len,
+    )
+    chunks = text_splitter.split_documents(data)
+    return chunks
 
-pages = loader.load_and_split()
-
-#%%
-latex_text = r"""
-\documentclass{article}
-
-\begin{document}
-
-\maketitle
-
-\section{Introduction}
-
-Large language models (LLMs) are a type of machine learning model that can be trained on vast amounts of text data to generate human-like language. In recent years, LLMs have made significant advances in various natural language processing tasks, including language translation, text generation, and sentiment analysis.
-
-\subsection{History of LLMs}
-
-The earliest LLMs were developed in the 1980s and 1990s, but they were limited by the amount of data that could be processed and the computational power available at the time. In the past decade, however, advances in hardware and software have made it possible to train LLMs on massive datasets, leading to significant improvements in performance.
-
-\subsection{Applications of LLMs}
-
-LLMs have many applications in the industry, including chatbots, content creation, and virtual assistants. They can also be used in academia for research in linguistics, psychology, and computational linguistics.
-
-\end{document}
-"""
-# %%
+## Vector db
+def vector_database(chunks):
+    embedding_model = watsonx_embedding()
+    vectordb = Chroma.from_documents(chunks, embedding_model)
+    return vectordb
 
 
-for i, page in enumerate(pages):
-    print(f"\n--- Page {i+1} ---\n")
-    print(page.page_content)
-
-# Section splitter
-sections = re.split(r'\\section{(.+?)}', latex_text)
-parsed_sections = {}
-for i in range(1, len(sections), 2):
-    title = sections[i]
-    content = sections[i + 1].strip()
-    parsed_sections[title] = content
-
-
-pprint(parsed_sections)
-
-
-
-#%%
-embed_params = {
+## Embedding model
+def watsonx_embedding():
+    embed_params = {
     EmbedTextParamsMetaNames.TRUNCATE_INPUT_TOKENS: 3,
     EmbedTextParamsMetaNames.RETURN_OPTIONS: {"input_text": True},
-}
+    }
 
-watsonx_embedding = WatsonxEmbeddings(
+    watsonx_embedding = WatsonxEmbeddings(
     model_id="ibm/slate-125m-english-rtrvr",
     url="https://eu-de.ml.cloud.ibm.com",
     project_id="bf575760-1fc7-46cf-9066-5a14330dd45b",
     params=embed_params,
-)
-# %%
-query = "How are you?"
+    )
+    return watsonx_embedding
 
-print(watsonx_embedding.embed_query(query))
+## LLM
+def get_llm():
+    model_id = 'mistralai/mixtral-8x7b-instruct-v01'
+    parameters = {
+        GenParams.MAX_NEW_TOKENS: 256,
+        GenParams.TEMPERATURE: 0.5,
+    }
+    watsonx_llm = WatsonxLLM(
+        model_id=model_id,
+        url="https://eu-de.ml.cloud.ibm.com",
+        project_id="bf575760-1fc7-46cf-9066-5a14330dd45b",
+        params=parameters,
+    )
+    return watsonx_llm
 
-#%%
-
-loader = TextLoader("new-Policies.txt")
-data = loader.load()
-
-text_splitter = RecursiveCharacterTextSplitter(
-    chunk_size=100,
-    chunk_overlap=20,
-    length_function=len,
-)
-
-chunks = text_splitter.split_documents(data)
-
-# %%
-ids = [str(i) for i in range(0, len(chunks))]
-vectordb = Chroma.from_documents(chunks, watsonx_embedding, ids=ids)
-
-query = "Smoking policy"
-docs = vectordb.similarity_search(query)
-print(docs)
-
-#%%
-query = "Email policy"
-
-retriever = vectordb.as_retriever(search_kwargs={"k": 1})
-docs = retriever.invoke(query)
-
-#%%
-model_id = 'mistralai/mixtral-8x7b-instruct-v01'
-llm_parameters = {
-    GenParams.MAX_NEW_TOKENS: 256,
-    GenParams.TEMPERATURE: 0.5,
-}
-watsonx_llm = WatsonxLLM(
-    model_id=model_id,
-    url="https://eu-de.ml.cloud.ibm.com",
-    project_id="bf575760-1fc7-46cf-9066-5a14330dd45b",
-    params=llm_parameters,
-)
-
-#%%
+## Retriever
+def retriever(file):
+    splits = document_loader(file)
+    chunks = text_splitter(splits)
+    vectordb = vector_database(chunks)
+    retriever = vectordb.as_retriever()
+    return retriever
 
 
 ## QA Chain
 def retriever_qa(file, query):
-    llm = watsonx_llm
+    llm = get_llm()
+    retriever_obj = retriever(file)
     qa = RetrievalQA.from_chain_type(llm=llm, 
                                     chain_type="stuff", 
-                                    retriever=retriever, 
+                                    retriever=retriever_obj, 
                                     return_source_documents=False)
     response = qa.invoke(query)
     return response['result']
 
-#%%
 rag_application = gr.Interface(
     fn=retriever_qa,
     allow_flagging="never",
